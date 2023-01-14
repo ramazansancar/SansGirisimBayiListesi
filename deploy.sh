2f2ccc0b1f3b7e14b89cc8ffed7b344c52350362
Ramazan Sancar <ramazansancar4545@gmail.com>
2023-01-14 12:47:33 +0000

    auto update added.

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+git config --global user.email $mail
+git config --global user.name $name
+git remote set-url origin https://$username:$token@github.com/$username/SansGirisimBayiListesi.git
+if git status --porcelain | grep -q '*'; then 
+	git add .
+	git commit -am 'update list'
+	echo $username | echo $token | git push -f origin HEAD:master
+else
+	echo 'No changes detected'
+fi
