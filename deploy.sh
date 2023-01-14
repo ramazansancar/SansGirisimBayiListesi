a43e3cf37415242b176c44b1ecb60f952265af6f
Ramazan Sancar <ramazansancar4545@gmail.com>
2023-01-14 13:14:08 +0000

    testing deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 git config --global user.email $mail
 git config --global user.name $name
 git remote set-url origin https://$username:$token@github.com/$username/SansGirisimBayiListesi.git
-if git status --porcelain | grep -q '*'; then 
+if git status --porcelain; then 
 	git add .
 	git commit -am 'update data'
 	echo $username | echo $token | git push -f origin HEAD:master
