4aa0ca549b40b4578ba8ec4661496b1b48f0f0ca
Ramazan Sancar <ramazansancar4545@gmail.com>
2023-01-14 13:05:13 +0000

    + Update code developed.

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ git config --global user.name $name
 git remote set-url origin https://$username:$token@github.com/$username/SansGirisimBayiListesi.git
 if git status --porcelain | grep -q '*'; then 
 	git add .
-	git commit -am 'update list'
+	git commit -am 'update data'
 	echo $username | echo $token | git push -f origin HEAD:master
 else
 	echo 'No changes detected'
